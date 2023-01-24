838e70bcdb81258c1276f25dc0fba49cc7858f66
Johnny Gérard <mail@jgerard.dev>
2023-01-24 03:11:09 +0100

    build: move files to public folder

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: cwd() });
-});
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`);
