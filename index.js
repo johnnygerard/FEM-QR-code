64be9913f29735172c10fca0613c1283ca37b8a7
Johnny Gérard <mail@jgerard.dev>
2023-01-25 15:01:22 +0100

    build: rename static folder to public

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-import { cwd } from 'node:process';
 import express from 'express';
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('docs'));
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`);
